4c2f3b0bb5d29f66e683a77d2588b0705289b1d6
domoticsduino <info@domoticsduino.cloud>
2025-03-05 17:22:02 +0100

    backup script for openhab 4 and influxdb 2

---
+++ README.md
@@ -0,0 +1,9 @@
+# OpenHAB4 Config and Support Tools
+
+## /backup-script/oh4-full-backup.sh
+
+Backup script for OpenHAB and Influxdb 2 (running on docker).
+
+**Credit to oliranks https://github.com/oliranks/openHAB3-full_backup_and_restore**
+
+For remote copy to work (using SCP), you have to copy ssh key from RPI to NAS, using ssh-copy-id script. See https://www.ssh.com/academy/ssh/copy-id
---
+++ backup-script/oh4-full-backup.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#Credits to https://github.com/oliranks/openHAB3-full_backup_and_restore
+
+TIMESTAMP="`date +%Y%m%d_%H%M%S`";
+BACKUPDIR="/data/backups/";
+BACKUPNAME="openhab-full-backup";
+INFLUXDB2CONFIGDIR="/var/lib/docker/volumes/influxdb_config_v2/_data"
+INFLUXDB2BACKUPDIR="/var/lib/docker/volumes/influxdb_backups_v2/_data/db"
+
+echo -e "\e[96m#############################\e[0m"
+echo -e "\e[96m##### \e[31mStopping services \e[96m#####\e[0m"
+echo -e "\e[96m#############################\e[0m"
+echo -e "Stopping openhab service..."
+sudo systemctl stop openhab.service
+echo -e "openhab service \e[31mstopped\e[0m."
+echo -e ""
+
+echo -e "\e[96m######################################\e[0m"
+echo -e "\e[96m##### \e[31mFolder setup \e[96m#####\e[0m"
+echo -e "\e[96m######################################\e[0m"
+echo -e "Creating openhab backup directory:";
+sudo mkdir -v "$BACKUPDIR$BACKUPNAME/openhab";
+echo -e "Creating influxdb backup directory:";
+sudo mkdir -v "$BACKUPDIR$BACKUPNAME/influxdb";
+echo -e ""
+
+echo -e "\e[96m###########################\e[0m"
+echo -e "\e[96m##### \e[31mopenHAB3 Backup \e[96m#####\e[0m"
+echo -e "\e[96m###########################\e[0m"
+echo -e "Creating openHAB3 backup..."
+sudo openhab-cli backup "$BACKUPDIR$BACKUPNAME/openhab/openhab-backup.zip"
+
+echo -e "\e[96m###########################\e[0m"
+echo -e "\e[96m##### \e[31mInfluxdb Backup \e[96m#####\e[0m"
+echo -e "\e[96m###########################\e[0m"
+echo -e "Copying influxdb config file..."
+sudo cp -arv "$INFLUXDB2CONFIGDIR" "$BACKUPDIR$BACKUPNAME/influxdb/"
+echo -e "Exporting influxdb databases..."
+sudo rm -Rf $INFLUXDB2BACKUPDIR
+sudo docker exec influxdb2 influx backup "/backups/db/"
+sudo mv $INFLUXDB2BACKUPDIR/ $BACKUPDIR$BACKUPNAME/influxdb/
+echo -e ""
+
+echo -e "\e[96m###############################\e[0m"
+echo -e "\e[96m##### \e[31mRestarting services \e[96m#####\e[0m"
+echo -e "\e[96m###############################\e[0m"
+echo -e "Starting openhab service..."
+sudo systemctl start openhab.service
+echo -e "openhab service \e[32mstarted\e[0m."
+echo -e ""
+
+echo -e "\e[96m##############################\e[0m"
+echo -e "\e[96m##### \e[31mCompressing backup \e[96m#####\e[0m"
+echo -e "\e[96m##############################\e[0m"
+cd $BACKUPDIR
+sudo tar cfvz $BACKUPDIR$BACKUPNAME.tar.gz $BACKUPNAME/
+echo -e ""
+
+echo -e "Backup finished!"
+echo -e ""
+
+echo -e "Total backup folder size:"
+sudo du -sh -- $BACKUPDIR$BACKUPNAME/*
+echo -e ""
+
+echo -e "Compressed backup file size:"
+sudo du -sh -- $BACKUPDIR$BACKUPNAME.tar.gz
+echo -e ""
+
+echo -e "\e[96m#######################\e[0m";
+echo -e "\e[96m##### \e[31mCleaning up \e[96m#####\e[0m";
+echo -e "\e[96m#######################\e[0m";
+sudo rm -rf $BACKUPDIR$BACKUPNAME/openhab
+sudo rm -rf $BACKUPDIR$BACKUPNAME/influxdb
+echo -e "Temporary directory deleted.";
+echo -e "";
+
+echo -e "Backup created \e[32msucessfully\e[0m! -> $BACKUPDIR$BACKUPNAME.tar.gz"
+echo -e "Move file on NAS using SCP"
+sudo scp $BACKUPDIR$BACKUPNAME.tar.gz sshd@192.168.1.6:/shares/Backup/Backup_OH4/$BACKUPNAME-$TIMESTAMP.tar.gz
+echo -e "SUCCESS"
